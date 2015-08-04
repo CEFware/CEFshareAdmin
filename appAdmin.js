37d9c055e3b7e6fed2d2fffb7ac6242e5f4dc700
Serhiy Khvashchuk <serhiy.khvashchuk@gmail.com>
2015-08-04 16:51:34 +0300

    stripe final get request fix

--- appAdmin.js
+++ appAdmin.js
@@ -7,7 +7,8 @@ Router.route('/stripe', function () {
     this.request.query['user_id']=str.slice(str.indexOf('/')+1, str.indexOf('http'));
     this.request.query.state=str.substr(0, str.indexOf('/'));
 
-console.log(str.substr(str.indexOf('http'),str.length+1));
-console.log(    HTTP.get(str.substr(str.indexOf('http'),str.length+1), {params: this.request.query, headers: this.request.headers}));
+//console.log(str.substr(str.indexOf('http'),str.length+1));
+//console.log(    HTTP.get(str.substr(str.indexOf('http'),str.length+1), {params: this.request.query, headers: this.request.headers}));
+    HTTP.get(str.substr(str.indexOf('http'),str.length+1), {params: this.request.query});
 
 }, {where:'server'});
